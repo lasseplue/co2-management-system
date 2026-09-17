42d1e9948b8e403aa373e38e362c461f97610239
laserganzok <93384296+laserganzok@users.noreply.github.com>
2026-09-17 15:01:18 +0200

    "Initial Spring Boot + Vaadin project setup"

---
+++ src/main/java/com/iu/co2management/co2_management_system/Co2ManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.iu.co2management.co2_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Co2ManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Co2ManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iu/co2management/co2_management_system/Co2ManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iu.co2management.co2_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Co2ManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
